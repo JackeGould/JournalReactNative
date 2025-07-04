35ddc41a6be2b7a4f4897d076874a5f147804445
RyanMoises5 <ryan.a.moises@gmail.com>
2025-07-04 13:51:24 -0400

    New posts can be created in the database, but needs context implementation

---
+++ server/models/Post.ts
@@ -0,0 +1,43 @@
+import mongoose, { Document, Schema, Types } from 'mongoose';
+
+export interface IPost extends Document {
+  _id: string;
+  title: string;
+  message: string;
+  // author: Schema.Types.ObjectId
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const PostSchema = new Schema<IPost>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 100
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+      // maxlength: 10000,
+    },
+    // author: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: 'User',
+    //   required: true
+    // }
+//  createdAt and updatedAt automatically created
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+)
+
+PostSchema.index({ author: 1, createdAt: -1 }); // For efficient author queries
+
+const Post = mongoose.model<IPost>('Post', PostSchema)
+
+export default Post;
--- server/models/User.ts
+++ server/models/User.ts
@@ -6,13 +6,35 @@ export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
+  // post: Schema.Types.ObjectId
 }
 
-const UserSchema = new Schema<IUser>({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new Schema<IUser>(
+  {
+    username: { 
+      type: String, 
+      required: true 
+    },
+    email: { 
+      type: String, 
+      required: true 
+    },
+    password: { 
+      type: String, 
+      required: true 
+    },
+    // post: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "Post",
+    //   required: false
+    // }
+  }, 
+  { 
+    toJSON: {
+      virtuals: true
+    },
+  }
+);
 
 const User = mongoose.model<IUser>('User', UserSchema);
 export default User;
